284dd4757a3bd42f8476c919cb8b9a98c8132d3f
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-10-07 12:10:56 +0530

    Time: 1 ms (99.85%), Space: 45.9 MB (50.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,9 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int[] dp= new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            dp[i]=0;
-        }
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
