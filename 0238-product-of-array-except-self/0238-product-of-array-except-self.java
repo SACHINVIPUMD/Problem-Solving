140498cacdb598d6c3997be163f61461070641f4
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-10-09 10:26:30 +0530

    Time: 3 ms (25.03%), Space: 55.6 MB (40.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] product = new int[nums.length];
-        int proval = 1;
-        boolean flag = false;
-        for(int num:nums){
-            if(flag && num==0){
-                proval = 0;
-            }
-            if(num==0){
-                flag=true;
-            }
-
+        int[] pre=new int[nums.length];
+        int[] suf=new int[nums.length];
+        int[] sol=new int[nums.length];
+        Arrays.fill(pre,1);
+        Arrays.fill(suf,1);
+        for(int i=1;i<nums.length;i++){
+            pre[i]=pre[i-1]*nums[i-1];
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            suf[i]=suf[i+1]*nums[i+1];
         }
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0)
-            proval*=nums[i];
+            sol[i]=pre[i]*suf[i];
         }
-
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                product[i]=proval;
-            }
-            else{
-                if(!flag)
-                product[i] = proval/nums[i];
-            }
-        }
-        return product;
+        return sol;
     }
 }
