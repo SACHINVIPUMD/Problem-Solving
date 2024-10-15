3ccfa8bf29be35e3b991be5937fbc193493dafd2
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-10-15 14:48:06 +0530

    Time: 8 ms (45.52%), Space: 45.7 MB (20.43%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -2,15 +2,15 @@ class Solution {
     public long minimumSteps(String s) {
         long sol = 0;
         int ocount = 0;
-        for(int i = 0 ; i < s.length() ; i++) {
-            if(s.charAt(i) == '0') ocount++;
-        }
-        for(int i = 0 ; i < s.length() ; i++) {
-            if(s.charAt(i) == '0') ocount--;
+        int n = s.length();
+        for(int i = n-1; i >= 0; i--) {
+            if(s.charAt(i) == '0') {
+                ocount++;
+            }
             else {
                 sol += ocount;
             }
-        }
+        }        
         return sol;
     }
 }
