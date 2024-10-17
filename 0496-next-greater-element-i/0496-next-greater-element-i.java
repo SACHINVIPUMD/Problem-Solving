7f166a0cda8c2dc13d7a68a2c9a4d4eed5bbacbf
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-10-17 22:18:21 +0530

    Time: 3 ms (92.61%), Space: 43.5 MB (57.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,33 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],i);
+        // Map to store the next greater element for each number in nums2
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // Stack to store elements in a decreasing order to find next greater elements
+        Stack<Integer> stack = new Stack<>();
+
+        // Traverse through nums2 array
+        for (int num : nums2) {
+            // While the current number is greater than the top of the stack
+            // it means the current number is the next greater element for the top element in the stack
+            while (!stack.isEmpty() && stack.peek() < num) {
+                // Pop the stack and record the next greater element in the map
+                map.put(stack.pop(), num);
+            }
+            // Push the current number onto the stack
+            stack.push(num);
         }
+
+        // For the remaining elements in the stack, there are no next greater elements
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+
+        // Prepare the result for nums1 based on the next greater elements stored in the map
         int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int num = nums1[i];
-            boolean found = false;
-            for(int j=map.get(nums1[i]);j<nums2.length;j++){
-                if(nums2[j]>num){
-                    ans[i]=nums2[j];
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                ans[i]=-1;
-            }
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
+
         return ans;
     }
 }
