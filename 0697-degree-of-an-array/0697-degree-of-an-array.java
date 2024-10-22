446ede843d891b861d7f38c98f88f4aa93675d42
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-10-22 10:21:05 +0530

    Time: 30 ms (37.4%), Space: 47.6 MB (47.84%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer,Integer> freq = new HashMap();
+        Map<Integer,Integer> firstIndx = new HashMap();
+        int n = nums.length;
+        int maxFreq = 0;
+        int minLen = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+            firstIndx.putIfAbsent(nums[i],i);
+            if(freq.get(nums[i])>maxFreq){
+                maxFreq = freq.get(nums[i]);
+                minLen = i-firstIndx.get(nums[i])+1;
+            }
+            else if(freq.get(nums[i]) == maxFreq){
+                minLen = Math.min(minLen,i-firstIndx.get(nums[i])+1);
+            }
+        }
+        return minLen;
+
+    }
+}
