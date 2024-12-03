da61232c367c04857f14b9e9c7d97e9bb47267d7
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-12-03 20:02:28 +0530

    Time: 0 ms (100%), Space: 44.4 MB (37.04%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,38 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode temphead = head;
+        ListNode temp = head;
         int count = 0;
-        
-        // Count the number of nodes in the list
-        while (temphead != null && count < k) {
-            temphead = temphead.next;
+        while(temp!=null && count<k){
+            temp = temp.next;
             count++;
         }
-        
-        // If there are less than k nodes remaining, return head (no reversal)
-        if (count < k) {
+        if(count<k){
             return head;
         }
-        
-        // Reverse the first k nodes
-        ListNode prev = null;
-        ListNode current = head;
-        ListNode next = null;
+
         count = 0;
-        while (current != null && count < k) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while(curr!=null && count<k){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
             count++;
         }
-        
-        // Recursively reverse the rest of the list
-        if (next != null) {
-            head.next = reverseKGroup(next, k);
+
+        if(next!=null){
+            head.next = reverseKGroup(next,k);
         }
-        
-        // 'prev' now points to the head of the reversed group
         return prev;
     }
 }
