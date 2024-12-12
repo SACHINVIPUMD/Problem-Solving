8c597103db3c86e4f65aca4884305d91a9566954
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-12-12 11:22:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (24.66%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -11,32 +11,26 @@ class MyQueue {
     }
     
     public int pop() {
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        int val = s2.pop();
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
-        }
-        
-        return val;
+        // int val = s2.pop();
+        return s2.pop();
     }
     
     public int peek() {
-        // return s2.peek();
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        int val = s2.peek();
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
-        }
-        
-        return val;
+        return s2.peek();
     }
     
     public boolean empty() {
-        return s1.isEmpty();
+        return s1.isEmpty()&&s2.isEmpty();
     }
 }
 
