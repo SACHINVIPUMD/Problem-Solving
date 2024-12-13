2640f85dd022d6408ad9ea53e980e3bd2c4e2d89
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-12-13 16:31:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.65%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,18 @@
 class Solution {
-    public void subseq(int ind,int[] nums,List<List<Integer>> ans,List<Integer> arr){
-        if(ind == nums.length){
-            ans.add(new ArrayList(arr));
-            return;
-        }
-        arr.add(nums[ind]);
-        subseq(ind+1,nums,ans,arr);
-        arr.remove(arr.size()-1);
-        subseq(ind+1,nums,ans,arr);
-    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
-        List<Integer> arr = new ArrayList();
-        subseq(0,nums,ans,arr);
+        List<Integer> list = new ArrayList();
+        recur(0,nums,list,ans);
         return ans;
+    }
+    public void recur(int ind,int[] nums,List<Integer> list,List<List<Integer>> ans){
+        if(ind == nums.length){
+            ans.add(new ArrayList(list));
+            return;
+        }
+        recur(ind+1,nums,list,ans);
+        list.add(nums[ind]);
+        recur(ind+1,nums,list,ans);
+        list.remove(list.size()-1);
     }
 }
