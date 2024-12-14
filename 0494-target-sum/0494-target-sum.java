f2247f535b5b0c7918155f94bdf13ccc6191fd58
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2024-12-14 11:59:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return recur(0,nums,target,0);
+        int[] memo = new int[nums.length+1];
+        Arrays.fill(nums,-1);
+        return recur(0,nums,target,0,memo);
     }
-    public static int recur(int ind,int[] nums,int target,int val){
+    public static int recur(int ind,int[] nums,int target,int val,int[] memo){
+        if(memo[ind]!=-1){
+            return memo[ind];
+        }
         if(ind == nums.length){
             if(val == target){
                 return 1;
             }
             return 0;
         }
-        return recur(ind+1,nums,target,val+nums[ind])+
-        recur(ind+1,nums,target,val-nums[ind]);
+        memo[ind] = recur(ind+1,nums,target,val+nums[ind],memo)+
+        recur(ind+1,nums,target,val-nums[ind],memo);
+        return memo[ind];
     }
 }
