7ee1ca6fce71df4e16667d76556654ac8fe7a88d
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2025-01-19 11:53:49 +0530

    Time: 1331 ms (14.16%), Space: 0B (100%) - LeetHub

---
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with rankCTE as (select *, rank() over (partition by product_id order by change_date desc) r
+from Products where change_date<='2019-08-16')
+
+select product_id,new_price as price from rankCTE where r = 1
+union
+select product_id,10 as price from Products
+where product_id not in (select product_id from rankCTE);
