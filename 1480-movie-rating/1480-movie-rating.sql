ba7287621f094ab6fbd1d2e1a6320b4beea5aa15
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2025-01-20 22:44:41 +0530

    Time: 3359 ms (5.18%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+(select name as results from MovieRating join Users using(user_id)
+group by user_id
+order by count(rating) desc,name limit 1)
+union all
+(select title as results from MovieRating join Movies using(movie_id)
+where created_at>='2020-02-01' and created_at<'2020-03-01'
+group by movie_id
+order by avg(rating) desc,title limit 1);
