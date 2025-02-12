43a28514a162da48422e14c8c3206de529f28499
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2025-02-12 22:05:15 +0530

    Time: 1 ms (98.72%), Space: 43.5 MB (46.62%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -23,34 +23,31 @@ class Pair{
 }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        int ans = 0;
-        if(root == null){
-            return ans;
-        }
         Queue<Pair> q = new LinkedList();
+        int depth = 0;
         q.offer(new Pair(root,0));
         while(!q.isEmpty()){
-            int size = q.size();
-            int min_id = q.peek().id;
             int first=0,last=0;
+            int size = q.size();
+            int minid = q.peek().id;
             for(int i=0;i<size;i++){
-                int cur_id = q.peek().id-min_id;
-                Pair front = q.poll();
-                if(front.node.left!=null){
-                    q.offer(new Pair(front.node.left,cur_id*2+1));
-                }
-                if(front.node.right!=null){
-                    q.offer(new Pair(front.node.right,cur_id*2+2));
-                }
+                int cur_id = q.peek().id-minid;
+                TreeNode cur_node = q.poll().node;
                 if(i==0){
                     first = cur_id;
                 }
                 if(i==size-1){
                     last = cur_id;
                 }
-                ans = Math.max(last-first+1,ans);
+                if(cur_node.left!=null){ 
+                    q.offer(new Pair(cur_node.left,2*cur_id+1));
+                } 
+                if(cur_node.right!=null){ 
+                    q.offer(new Pair(cur_node.right,2*cur_id+2));
+                } 
+                depth = Math.max(depth,last-first+1);
             }
         }
-        return ans;
+        return depth;
     }
 }
