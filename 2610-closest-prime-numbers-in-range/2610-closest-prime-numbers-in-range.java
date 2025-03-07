21028c53849d71580115734d93028dfabb5fb6e8
SACHIN VIPU M D <125341443+SACHINVIPUMD@users.noreply.github.com>
2025-03-07 19:18:27 +0530

    Time: 90 ms (71.2%), Space: 50.9 MB (22.8%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -22,12 +22,12 @@ class Solution {
         return ans;
     }
     private boolean[] sieve(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime,true);
         isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i*i <= n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j = i*i; j<=n;j+=i){
                     isPrime[j] = false;
                 }
             }
